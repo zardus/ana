4797ab406db6ba2a94ec8c57c543f7951a0213a3
Yan <yans@yancomm.net>
2014-10-14 21:15:53 -0700

    add set_dl to set the global datalayer instance

--- ana/__init__.py
+++ ana/__init__.py
@@ -3,6 +3,9 @@ from .datalayer import *
 dl = DataLayer()
 def get_dl():
     return dl
+def set_dl(*args, **kwargs):
+    global dl
+    dl = DataLayer(*args, **kwargs)
 
 class M(object):
     '''This is a marker that's used internally by ANA.'''
